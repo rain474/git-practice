81a77c62a9cfded02411e4be786e60d0ee3d4fc2
rain474 <rainierpdecastro@su.edu.ph>
2026-08-01 16:16:04 +0800

    modified database

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,7 @@
 import os
 from dotenv import load_dotenv
-from supabase import create_client, Client 
 
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
-
-supabase: Client = create_client(url, key)
+print("URL:", os.getenv("SUPABASE_URL"))
+print("KEY exists:", os.getenv("SUPABASE_KEY") is not None)
